985b179e619fd336c649abb5bb6158e06490042e
rafimchmd <rafimfahreza@gmail.com>
2022-04-07 10:55:55 +0700

    initial commit

---
+++ Sources/Greeting.swift
@@ -0,0 +1,17 @@
+//
+//  File.swift
+//  
+//
+//  Created by Rafi Mochamad Fahreza on 07/04/22.
+//
+
+import Foundation
+import UIKit
+
+public class LiveCamera {
+    
+    func openCamera() {
+        print("OPENING LIVENESS CAMERA CHECK")
+    }
+    
+}
